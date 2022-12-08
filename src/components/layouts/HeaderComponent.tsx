15e7150d2b03f6a5493f5d2a7e431ca4778e8eb8
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-12-08 21:39:47 +0700

    style(HeaderComponent): :lipstick: adjust header styling

--- src/components/layouts/HeaderComponent.tsx
+++ src/components/layouts/HeaderComponent.tsx
@@ -15,11 +15,17 @@ const HeaderComponent = () => {
   };
 
   return (
-    <header className="fixed top-0 z-50 w-full bg-white p-0.5 opacity-90 dark:bg-darkpurple-700">
+    <header
+      className={clsxm(
+        "fixed top-0 z-50 w-full bg-white/10 p-0.5",
+        "shadow-sm shadow-darkpurple-50 backdrop-blur-sm",
+        "dark:bg-darkpurple-700/10 dark:shadow-darkpurple-700"
+      )}
+    >
       <div
         className={clsx(
           "flex items-center justify-between",
-          "mx-auto h-14 max-w-5xl px-2 md:px-1"
+          "mx-auto h-14 max-w-2xl px-2 md:px-1"
         )}
       >
         <h5 className="font-bold text-black dark:text-white">QR Generator</h5>
