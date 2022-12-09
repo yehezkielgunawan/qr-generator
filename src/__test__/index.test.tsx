e3ab1575ed99177a8969957b851428c49899dda5
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2022-12-09 20:20:44 +0700

    test(test file): :white_check_mark: adjust test structure
    Will be updated soon by adding userEvent unit test

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -9,15 +9,14 @@ jest.mock("react-qrcode-logo", () => ({
   QRCode: jest.fn(),
 }));
 
-describe("Render Page Normally", () => {
-  it("Renders Home Page Successfully", () => {
-    render(<Home />);
-    const heading = screen.getByText("QR Generator");
-    expect(heading).toBeInTheDocument();
-  });
-  it("Renders 404 page successfully!", () => {
-    render(<NotFoundPage />);
-    const heading = screen.getByText("Page Not Found");
-    expect(heading).toBeInTheDocument();
-  });
+test("Renders Home Page Successfully", () => {
+  render(<Home />);
+  const heading = screen.getByText("QR Generator");
+  expect(heading).toBeInTheDocument();
+});
+
+test("Renders 404 page successfully!", () => {
+  render(<NotFoundPage />);
+  const heading = screen.getByText("Page Not Found");
+  expect(heading).toBeInTheDocument();
 });
