da686db16131c6c30d2a86b8480eebe936e521c6
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-03-23 17:12:04 +0700

    style: apply consistent formatting across multiple components

--- src/components/links/UnderlineLink.tsx
+++ src/components/links/UnderlineLink.tsx
@@ -3,19 +3,19 @@ import clsxm from "@/lib/helpers/clsxm";
 import UnstyledLink, { UnstyledLinkProps } from "./UnstyledLink";
 
 function UnderlineLink({ children, className, ...rest }: UnstyledLinkProps) {
-  return (
-    <UnstyledLink
-      {...rest}
-      className={clsxm(
-        "link-underline",
-        "inline-flex items-center",
-        "focus-visible:dashed focus:outline-hidden focus-visible:ring-3 focus-visible:ring-primary-500 focus-visible:ring-offset-2",
-        className
-      )}
-    >
-      {children}
-    </UnstyledLink>
-  );
+	return (
+		<UnstyledLink
+			{...rest}
+			className={clsxm(
+				"link-underline",
+				"inline-flex items-center",
+				"focus-visible:dashed focus:outline-hidden focus-visible:ring-3 focus-visible:ring-primary-500 focus-visible:ring-offset-2",
+				className,
+			)}
+		>
+			{children}
+		</UnstyledLink>
+	);
 }
 
 export default UnderlineLink;
--- src/lib/helpers/clsxm.ts
+++ src/lib/helpers/clsxm.ts
@@ -3,5 +3,5 @@ import { twMerge } from "tailwind-merge";
 
 /** Merge classes with tailwind-merge with clsx full feature */
 export default function clsxm(...classes: ClassValue[]) {
-  return twMerge(clsx(...classes));
+	return twMerge(clsx(...classes));
 }
