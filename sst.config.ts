ba2ac9bb0e2c63cc68a5c2fa09a5f205bb453324
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-06-11 20:09:54 +0700

    feat: add SST config file for app deployment

---
+++ sst.config.ts
@@ -0,0 +1,15 @@
+/// <reference path="./.sst/platform/config.d.ts" />
+
+export default $config({
+  app(input) {
+    return {
+      name: "qr-generator",
+      removal: input?.stage === "production" ? "retain" : "remove",
+      protect: ["production"].includes(input?.stage),
+      home: "aws",
+    };
+  },
+  async run() {
+    new sst.aws.Nextjs("MyWeb");
+  },
+});
