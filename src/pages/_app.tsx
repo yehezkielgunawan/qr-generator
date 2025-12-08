ba1d60eb2053122ffcec844a9266269cfeedd611
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-12-08 21:28:53 +0700

    fix: Adjust the rendering method

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,30 @@
 import "@/styles/globals.css";
 import { DefaultSeo } from "next-seo";
-import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import "@fontsource/catamaran";
+import dynamic from "next/dynamic";
 
 import defaultSEOConfig from "../../next-seo.config";
 
+const ThemeProvider = dynamic(
+  () => import("next-themes").then((mod) => mod.ThemeProvider),
+  { ssr: false }
+);
+
 function MyApp({ Component, pageProps }: AppProps) {
-	return (
-		<ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
-			<Head>
-				<meta
-					name="viewport"
-					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-				/>
-			</Head>
-			<DefaultSeo {...defaultSEOConfig} />
-			<Component {...pageProps} />
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <Head>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+        />
+      </Head>
+      <DefaultSeo {...defaultSEOConfig} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default MyApp;
